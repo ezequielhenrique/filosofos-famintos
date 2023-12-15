627ce602e068f109db1915150a8939ccb06036a7
ezequielhenrique <ezequielhenrique806@gmail.com>
2023-12-15 19:41:57 -0300

    correção da lógica de implementação

--- src/Filosofo.java
+++ src/Filosofo.java
@@ -1,18 +1,13 @@
 public class Filosofo extends Thread {
     final int tempo = 1000;
-    final int PENSANDO = 0;
-    final int COMENDO = 1;
-    final int FAMINTO = 2;
     String nome;
     Mesa mesa;
     int id;
-    int estado;
 
     Filosofo (String nome, Mesa mesa, int id) {
         this.nome = nome;
         this.mesa = mesa;
         this.id = id;
-        this.estado = PENSANDO;
 
         System.out.println("O filósofo " + nome + " sentou-se à mesa");
     }
@@ -23,26 +18,22 @@ public class Filosofo extends Thread {
 
     public void pensar() {
         try {
-            this.estado = PENSANDO;
             System.out.println("O filófofo " + getNome() + " está PENSANDO...");
             Thread.sleep(tempo);
 
         } catch (InterruptedException ex) {
             System.out.println(ex.getMessage());
         }
-
     }
 
     public void comFome() {
-        this.estado = FAMINTO;
-        mesa.estadoFilosofos[id] = FAMINTO;
         System.out.println("O filósofo " + getNome() + " está FAMINTO...");
     }
 
     public void comer() {
         try {
-            this.estado = COMENDO;
             System.out.println("O filófofo " + getNome() + " está COMENDO...");
+            mesa.status();
             Thread.sleep(tempo);
 
         } catch (InterruptedException ex) {
@@ -51,46 +42,22 @@ public class Filosofo extends Thread {
     }
 
     public void pegarGarfo() {
-        comFome();
-        tentaObterGarfos();
+        mesa.pegarGarfos(id);
     }
 
     public void largarGarfo() {
-        pensar();
-        mesa.estadoFilosofos[id] = PENSANDO;
-    }
-
-    public void tentaObterGarfos() {
-        if (mesa.estadoFilosofos[vizinhoEsquerda()] != 1 && mesa.estadoFilosofos[vizinhoDireita()] != 1) {
-            comer();
-            mesa.estadoFilosofos[id] = COMENDO;
-        }
-    }
-
-    private int vizinhoEsquerda() {
-        if (this.id == 0) {
-            return 4;
-        } else {
-            return this.id - 1;
-        }
-    }
-
-    private int vizinhoDireita() {
-        if (this.id == 4) {
-            return 0;
-        } else {
-            return this.id;
-        }
+        mesa.largarGarfos(this.id);
     }
 
     @Override
     public void run() {
         try {
-            pensar();
-
             while (true) {
+                Thread.sleep(tempo);
+                pensar();
                 pegarGarfo();
                 Thread.sleep(tempo);
+                comer();
                 largarGarfo();
             }
 
--- src/Mesa.java
+++ src/Mesa.java
@@ -1,21 +1,51 @@
+import java.util.Arrays;
+
 public class Mesa {
     final static int PENSANDO = 0;
     final static int COMENDO = 1;
     final static int FAMINTO = 2;
     final static int NUM_FILOSOFOS = 5;
-    final static int PRIMEIRO_FILOSOFO = 0;
-    final static int ULTIMO_FILOSOFO = NUM_FILOSOFOS - 1;
-    boolean[] garfos = new boolean[NUM_FILOSOFOS];
-    int[] estadoFilosofos = new int[NUM_FILOSOFOS];
-    int[] tentativas = new int[NUM_FILOSOFOS];
+    public int[] estadoFilosofos = new int[NUM_FILOSOFOS];
+    public int[] tentativas = new int[NUM_FILOSOFOS];
 
     public Mesa()
     {
-        for (int i = 0; i < 5; i++)
-        {
-            garfos[i] = true;
+        for (int i = 0; i < NUM_FILOSOFOS; i++) {
             estadoFilosofos[i] = PENSANDO;
             tentativas[i] = 0;
         }
+    }
+
+    public void pegarGarfos(int id) {
+        this.estadoFilosofos[id] = FAMINTO;
+
+        if (this.estadoFilosofos[vizinhoEsquerda(id)] != COMENDO  && this.estadoFilosofos[vizinhoDireita(id)] != COMENDO) {
+            this.estadoFilosofos[id] = COMENDO;
+            this.tentativas[id] ++;
+        }
+    }
+
+    public void largarGarfos(int id) {
+        this.estadoFilosofos[id] = PENSANDO;
+    }
+
+    private int vizinhoEsquerda(int id) {
+        if (id == 0) {
+            return 4;
+        } else {
+            return id - 1;
+        }
+    }
+
+    private int vizinhoDireita(int id) {
+        if (id == 4) {
+            return 0;
+        } else {
+            return id + 1;
+        }
+    }
+    public void status() {
+        System.out.println("0-Pensando/1-Comendo/2-Faminto: " + Arrays.toString(estadoFilosofos));
+        System.out.println("Vezes que cada filosófo comeu: " + Arrays.toString(tentativas));
     }
 }
